97114890421f89b8143e2c08b3c476bfaf58cf52
t.sarmis <t.sarmis@kaizengaming.com>
2025-08-22 16:26:13 +0300

    add artificial delay to /weatherforecast endpoint

--- src/weatherapp/Program.cs
+++ src/weatherapp/Program.cs
@@ -32,7 +32,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
@@ -42,6 +42,9 @@ app.MapGet("/weatherforecast", () =>
             summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
+
+    await Task.Delay(Random.Shared.Next(1000));
+
     return forecast;
 })
 .WithName("GetWeatherForecast");
